3e2636ffbe8f44ff0d6e423f2a58de34c87614b7
Zhao KK <kikakkz@hotmail.com>
2023-05-09 07:19:30 +0000

    Support to query app user when update

--- pkg/oplog/appuser/query.go
+++ pkg/oplog/appuser/query.go
@@ -1,9 +1,37 @@
+//nolint
 package appuser
 
 import (
 	"context"
+	"fmt"
+	// "encoding/json"
+	"path/filepath"
+	"strings"
+	// appmwcli "github.com/NpoolPlatform/appuser-middleware/pkg/client/app"
+	// npool "github.com/NpoolPlatform/message/npool/appuser/gw/v1/app"
 )
 
 func GetEntity(ctx context.Context, path, args string) (interface{}, error) {
+	strs := strings.Split("path", "/")
+	if len(strs) < 4 {
+		return nil, fmt.Errorf("invalid path")
+	}
+	if !strings.HasPrefix(strs[3], "update") {
+		return nil, nil
+	}
+
+	_path := filepath.Join(strs[3:]...)
+	switch _path {
+	case "/update/app":
+		/*
+			var req npool.UpdateAppRequest
+			if err := json.Unmarshal([]byte(args), &req); err != nil {
+				return nil, err
+			}
+			return appmwcli.GetApp(ctx, req.ID)
+		*/
+	default:
+	}
+
 	return nil, nil
 }
