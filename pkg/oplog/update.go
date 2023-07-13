b0fa97f176b30a49b54be42b89b4e351e54189b9
Zhao KK <kikakkz@hotmail.com>
2023-07-13 06:07:34 +0000

    If result is array, just record how many items got

--- pkg/oplog/update.go
+++ pkg/oplog/update.go
@@ -2,12 +2,26 @@ package oplog
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 
 	oplogmwpb "github.com/NpoolPlatform/message/npool/oplog/mw/v1/oplog"
 	oplogmwcli "github.com/NpoolPlatform/oplog-middleware/pkg/client/oplog"
 )
 
 func (h *Handler) UpdateOpLog(ctx context.Context) (*oplogmwpb.OpLog, error) {
+	if h.EntID == nil {
+		return nil, fmt.Errorf("invalid entid")
+	}
+
+	if h.NewValue != nil {
+		var arr []interface{}
+		if err := json.Unmarshal([]byte(*h.NewValue), &arr); err == nil {
+			value := fmt.Sprintf(`{"Infos":{"Count":%v}}`, len(arr))
+			h.NewValue = &value
+		}
+	}
+
 	return oplogmwcli.UpdateOpLog(ctx, &oplogmwpb.OpLogReq{
 		EntID:            h.EntID,
 		NewValue:         h.NewValue,
